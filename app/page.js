fbbaa85733cf0e096fd9af7c5a541e1ac5c785ca
iwansal64 <iwansal64@gmail.com>
2024-02-17 09:16:07 +0700

    Adding some modification to server side of main page so that it can verify user first before go into the page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+'use server'
 import styles from "./page.module.css";
+import { must_login } from "@/server_functionalities/server_security";
 
-export default function Home() {
+export default async function Home() {
+  await must_login();
+
   return (
     <>
 
