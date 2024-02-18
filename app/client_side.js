c5feaecc419cb93411c8a36e033ab52b03fac63f
iwansal64 <iwansal64@gmail.com>
2024-02-18 17:07:45 +0700

    Make navigation bar more beautiful and fast
    Fast means faster performance to process and render the navigation bar

--- app/client_side.js
+++ app/client_side.js
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import styles from "./page.module.css"
 import Link from "next/link";
+import { useSearchParams } from "next/navigation"
+import anime from "animejs";
+import { useEffect, useMemo } from "react";
 
 function Logo() {
 
@@ -25,9 +28,78 @@ function Profile() {
 
 }
 
-export function NavBar() {
+export function NavBar({ active_index = 0 }) {
+    const params = useSearchParams();
+    let from = undefined;
 
+    if (params.has("from")) {
+        from = Number.parseInt(params.get("from"));
+    }
 
+    const links = useMemo(() => {
+        return {
+            "Home": "/",
+            "Explore": "/explore",
+            "Message": "/message",
+            "Community": "/community"
+        }
+    }, []);
+
+    useEffect(() => {
+        const link_container_element = document.getElementsByClassName(styles.navigation_side)[0];
+
+        const link_elements = document.getElementsByClassName(styles.link);
+        const active_link_element = link_elements[active_index];
+        const not_active_link_elements = Array.from(link_elements).filter((value) => value != active_link_element);
+
+        if (typeof from != "undefined" && !isNaN(from) && from < Object.keys(links).length) {
+            // Adding A Not Active Bg Element For Non Active Link
+            Array.from(link_elements).forEach((link_element) => {
+                link_element.children[1].classList.remove(styles.not_visible);
+            })
+
+            active_link_element.children[1].classList.add(styles.not_visible);
+
+            // Adding An Active Bg Element For Ex Active Link
+            const ex_active_link_element = link_elements[from];
+
+            const offset_top = ex_active_link_element.children[0].parentElement.offsetTop;
+            const bg_active_element = document.createElement("div");
+
+            bg_active_element.className = styles.bg_active;
+            bg_active_element.style.top = offset_top + "px";
+            bg_active_element.id = "bg_active_element_moving";
+
+            link_container_element.appendChild(bg_active_element);
+
+            const target_top = active_link_element.children[0].parentElement.offsetTop;
+
+            // Animate The Active Bg Element For Current Active Link
+            anime({
+                targets: "#bg_active_element_moving",
+                duration: 200,
+                top: target_top + 'px',
+                easing: 'easeInOutQuad'
+            });
+        }
+        else {
+            const offset_top = active_link_element.children[0].parentElement.offsetTop;
+            const bg_active_element = document.createElement("div");
+
+            bg_active_element.className = styles.bg_active;
+            bg_active_element.style.top = offset_top + "px";
+
+            link_container_element.appendChild(bg_active_element);
+
+            console.log(offset_top);
+
+            not_active_link_elements.forEach((not_active_link_element) => {
+                not_active_link_element.children[1].classList.remove(styles.not_visible);
+            })
+
+            active_link_element.children[1].classList.add(styles.not_visible);
+        }
+    }, [active_index, from, links])
 
     return (
         <>
@@ -37,10 +109,12 @@ export function NavBar() {
                         <Logo />
                     </div>
                     <div className={styles.navigation_side}>
-                        <Link className={`${styles.link} ${styles.active}`} href="/">Home</Link>
-                        <Link className={`${styles.link}`} href="/explore">Explore</Link>
-                        <Link className={`${styles.link}`} href="/message">Message</Link>
-                        <Link className={`${styles.link}`} href="/community">Community</Link>
+                        {Object.entries(links).map(([text, link], index) =>
+                            <Link key={index} className={`${styles.link} ${active_index == index ? styles.active : ''}`} href={link + "?from=" + active_index}>{text}
+                                <div className={styles.bg_tracker}></div>
+                                <div className={styles.bg_notactive + " " + styles.not_visible}></div>
+                            </Link>
+                        )}
                     </div>
                 </div>
                 <div className={styles.lower_side}>
@@ -51,5 +125,4 @@ export function NavBar() {
             </nav>
         </>
     )
-
 }
