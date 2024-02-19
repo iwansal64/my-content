27873c964b47f55e5b01598174f81a2e0ad70bbc
iwansal64 <iwansal64@gmail.com>
2024-02-19 21:30:51 +0700

    Create logout button using server_side_button

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import styles from "./page.module.css";
 import { must_login } from "@/server_functionalities/server_security";
 import NavBar from "./global_components/navbar";
 import StatusesSection from "./_components/statuses_section";
+import ServerSideBtn from "./global_components/server_side_btn";
+import { logout } from "./global_functions/user_manager_functions";
 
 export default async function Home() {
   await must_login();
@@ -15,6 +17,7 @@ export default async function Home() {
       <div className={styles.home_container}>
         <StatusesSection />
       </div>
+      <ServerSideBtn callback={logout} />
     </>
   );
 }
