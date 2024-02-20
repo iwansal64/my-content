b65c4c3b7a567f8e3725c9c0a2ace2e57aaca14e
iwansal64 <iwansal64@gmail.com>
2024-02-20 21:32:38 +0700

    Modification (minor) for global.css and /page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import styles from "./page.module.css";
 import { must_login } from "@/server_functionalities/server_security";
 import NavBar from "./global_components/navbar";
-import StatusesSection from "./_components/statuses_section";
+import PostContainer from "./_components/post_container";
 import ServerSideBtn from "./global_components/server_side_btn";
 import { logout } from "./global_functions/user_manager_functions";
 
@@ -15,7 +15,7 @@ export default async function Home() {
         <NavBar />
       </div>
       <div className={styles.home_container}>
-        <StatusesSection />
+        <PostContainer />
       </div>
       <ServerSideBtn callback={logout} />
     </>
