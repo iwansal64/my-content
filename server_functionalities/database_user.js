ac1d27ffc6ffc75df003f61cc1d31587d3a994e5
iwansal64 <iwansal64@gmail.com>
2024-02-21 19:27:35 +0700

    Modification to server database function return value

--- server_functionalities/database_user.js
+++ server_functionalities/database_user.js
@@ -18,7 +18,11 @@ export async function get_users({ params = {}, match_all = false }) {
     return {
         "success": true,
         "status_code": 200,
-        "result": result
+        "message": "Successfully get data!",
+        "result": {
+            "total": match_all ? result.length : (result ? 1 : 0),
+            "data": result
+        }
     };
 }
 
@@ -29,23 +33,30 @@ export async function update_users({ params, new_data, match_all = false }) {
         return {
             "success": false,
             "status_code": 400,
-            "result": []
+            "message": "",
+            "result": {
+                "data": []
+            }
         };
     }
 
     let result;
 
     if (match_all) {
-        result = user_database.updateMany(params, new_data);
+        result = await user_database.updateMany(params, new_data);
     }
     else {
-        result = user_database.updateOne(params, new_data);
+        result = await user_database.updateOne(params, new_data);
     }
 
     return {
         "success": true,
         "status_code": 200,
-        "result": result
+        "message": "",
+        "result": {
+            "total": result.matchedCount,
+            "data": result
+        }
     }
 }
 
@@ -56,27 +67,58 @@ export async function delete_users({ params, match_all }) {
         return {
             "success": false,
             "status_code": 400,
-            "result": []
+            "message": "",
+            "result": {
+                "data": []
+            }
         };
     }
 
     let result;
 
     if (match_all) {
-        result = user_database.updateMany(params, new_data);
+        result = await user_database.deleteMany(params);
     }
     else {
-        result = user_database.updateOne(params, new_data);
+        result = await user_database.deleteOne(params);
     }
 
     return {
         "success": true,
         "status_code": 200,
-        "result": result
+        "message": `Total ${result.deletedCount}`,
+        "result": {
+            "total": result.deletedCount,
+            "data": result
+        }
     }
 }
 
 export async function insert_users({ new_data }) {
+    const user_database = await get_user_database();
 
+    if (!new_data) {
+        return {
+            "success": false,
+            "status_code": 400,
+            "message": "New Data Required",
+            "result": {
+                "total": -1,
+                "data": []
+            }
+        }
+    }
+
+    const result = await user_database.insertOne(new_data);
+
+    return {
+        "success": true,
+        "status_code": 200,
+        "message": result.insertedId ? "Successfully Insert Data!" : "Unsuccessfully Insert Data!",
+        "result": {
+            "total": result.insertedId ? 1 : 0,
+            "data": result
+        }
+    }
 }
 
