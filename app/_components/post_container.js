09373b5897087e8722b91071aef6e2bfc416690d
iwansal64 <iwansal64@gmail.com>
2024-02-24 10:50:36 +0700

    Fix Hydration Error :)

--- app/_components/post_container.js
+++ app/_components/post_container.js
@@ -2,15 +2,6 @@
 import styles from "../page.module.css"
 import Link from "next/link"
 
-function Status({ username, profile }) {
-
-    return (
-        <div className={styles.status}>
-            <h1></h1>
-        </div>
-    )
-}
-
 export default function Post({ data, user }) {
     return (
         <>
--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ import { get_posts } from "@/server_functionalities/database_post"
 import { get_users } from "@/server_functionalities/database_user";
 import Post from "./_components/post_container";
 import { ObjectId } from "mongodb";
+import { show_message } from "./client_functionalities/ui_functions";
+
 
 export default async function Home() {
   await must_login();
@@ -15,9 +17,17 @@ export default async function Home() {
   const { success, message, result } = await get_posts({});
   result["data"] = JSON.parse(result["data"]);
 
+  for (let index = 0; index < result["total"]; index++) {
+    const data = result["data"][index];
+    const creator = JSON.parse((await get_users({ params: { "_id": (new ObjectId(data["creator_id"])) }, match_all: false }))["result"]["data"]);
+    result["data"][index] = [data, creator];
+  };
+
   if (!success) {
-    alert(message);
+    show_message({ message });
   }
+
+  console.log(result["data"]);
 
   return (
     <>
@@ -26,10 +36,12 @@ export default async function Home() {
       </div>
       <div className={styles.home_container}>
         <div className={styles.post_container}>
-          {result["data"].map(async (data, index) => {
-            const creator = JSON.parse((await get_users({ params: { "_id": (new ObjectId(data["creator_id"])) }, match_all: false }))["result"]["data"]);
+          {result["data"].map((value, index) => {
+            const data = value[0];
+            const user = value[1];
+
             return (
-              <Post key={index} data={data} user={creator} />
+              <Post key={index} data={data} user={user} />
             );
           })}
         </div>
