c8da820fc89bf868f19ac1ce4f1f5ea62e930454
iwansal64 <iwansal64@gmail.com>
2024-02-24 19:14:12 +0700

    Adapt some pages for the new navigation (add username under the profile icon)

--- app/message/page.js
+++ app/message/page.js
@@ -1,12 +1,14 @@
 "use server"
 
 import NavBar from "../global_components/navbar"
+import { must_login } from "@/server_functionalities/server_security";
 
 export default async function Message() {
+    const [username, password] = (await must_login()).split(":", 2);
 
     return (
         <>
-            <NavBar active_index={2} />
+            <NavBar username={username} active_index={2} />
 
         </>
     )
