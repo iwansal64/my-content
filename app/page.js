903e97c596728e6eb4b5b563fa5f4eeb8619d4a6
iwansal64 <iwansal64@gmail.com>
2024-02-25 12:39:29 +0700

    Add styles for post page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import { must_login } from "@/server_functionalities/server_security";
 import NavBar from "./global_components/navbar";
 import ServerSideBtn from "./global_components/server_side_btn";
-import { logout } from "./client_functionalities/user_manager_functions";
 import { get_posts } from "@/server_functionalities/database_post"
 import { get_users } from "@/server_functionalities/database_user";
 import Post from "./_components/post_container";
@@ -13,6 +12,7 @@ import ServerSideMessage from "./global_components/server_side_message";
 import MessageContainer from "./global_components/message_container";
 import { Suspense } from "react";
 import { PostPlaceHolder } from "./_components/post_container";
+import { logout } from "./client_functionalities/user_manager_functions";
 
 async function PostContainer() {
   const { success, message, result } = await get_posts({});
@@ -43,7 +43,9 @@ async function PostContainer() {
 }
 
 export default async function Home() {
-  const [username, password] = (await must_login()).split(":", 2);
+  const [username, password] = await must_login();
+  console.log(username);
+  console.log(password);
 
 
   return (
@@ -58,6 +60,7 @@ export default async function Home() {
       </div>
       <ServerSideMessage message={"Welcome " + username + "!"} duration={2000} />
       <MessageContainer />
+      <ServerSideBtn callback={logout} text="Logout" />
     </>
   );
 }
