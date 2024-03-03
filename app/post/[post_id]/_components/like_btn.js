95ecbf823cd49948920c7694076c87d3a7ab57e7
IwanTz <iwansal64@gmail.com>
2024-03-03 09:49:28 +0700

    Now user know if the button is like the post or not

--- app/post/[post_id]/_components/like_btn.js
+++ app/post/[post_id]/_components/like_btn.js
@@ -2,20 +2,22 @@
 import { useState } from "react";
 import { handle_like_post } from "@/app/client_functionalities/user_manager_functions"
 import styles from "../../post.module.css";
+import { show_message } from "@/app/client_functionalities/ui_functions";
 
 export default function LikeBtn({ liked = false, user_id, post_id }) {
     const [button_class, set_button_class] = useState((liked ? styles.liked : styles.not_liked));
 
     const handle_click = () => {
         handle_like_post({ user_id, post_id });
+        const message = button_class == styles.liked ? "Unlike the post!" : "Like the post!";
         set_button_class(button_class == styles.liked ? styles.not_liked : styles.liked);
+        show_message({ message, duration: 1000 });
     };
 
 
     return (
         <>
-            {/* <ServerSideButton text="Like" callback={handle_like_post} params={{ user_id, post_id }} class_name={(result["result"]["total"] > 0 ? styles.liked : styles.not_liked)} /> */}
-            <button onClick={handle_click} className={button_class}>Like</button>
+            <button onClick={handle_click} className={button_class + " " + styles.like_btn}>{button_class == styles.liked ? "Liked" : "Like"}</button>
         </>
     )
 }
